8bb0787835c641abf11963d8417a82c0699a3df3
Emily Lillie <emilylillie3@gmail.com>
2019-09-08 16:15:47 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# makes sure user cant sign up without name, email, and pw 
+
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
 end
---
+++ db/migrate/createusers.rb
@@ -0,0 +1,11 @@
+# creates a users table with columns for name, email and pw 
+
+class Createusers < ActiveRecord::Migration
+   def change
+     create_table :users do |t|
+       t.string :name
+       t.string :email
+       t.string :password
+     end
+   end
+ end
