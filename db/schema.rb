8d8a39b213651eea21e7a7e0b607b114563a07f8
Emily Lillie <emilylillie3@gmail.com>
2019-09-08 17:00:34 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190827184205) do
+ActiveRecord::Schema.define(version: 0) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
