1d3cca3b84721e0712fba5eabbe54f8ff5cbf52c
Emily Lillie <emilylillie3@gmail.com>
2019-09-08 16:33:45 +0000

    Automatically backed up by Learn

--- db/migrate/createusers.rb
+++ db/migrate/createusers.rb
@@ -1,6 +1,9 @@
 # creates a users table with columns for name, email and pw 
 
 class Createusers < ActiveRecord::Migration
+  require_relative './config/environment' 
+   
+   
    def change
      create_table :users do |t|
        t.string :name
